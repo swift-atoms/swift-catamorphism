a11ecacabd0bb68a8f4d4be2c538bf752bf3d5f4
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-17 13:26:45 +0200

    Point dependencies at their new organizations (algebra packages in swift-atoms; interface and client in swift-molecules)

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "Catamorphism Macro Core", targets: ["Catamorphism Macro Core"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-molecules/swift-recursive.git", branch: "main"),
+        .package(url: "https://github.com/swift-atoms/swift-recursive.git", branch: "main"),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", "603.0.2"..<"604.0.0"),
     ],
     targets: [
